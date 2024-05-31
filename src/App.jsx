1c8feb1e5108e3df181f481d6c16d9d008c5b6de
Honey Sebastian <honeysebastian2000@gmail.com>
2024-05-31 21:49:01 +0530

    Todo App Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, toggleTodo, deleteTodo } from './redux/todoSlice';
+import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  const handleAddTodo = () => {
+    if (text.trim()) {
+      dispatch(addTodo(text));
+      setText('');
+    }
+  };
+
+  const completedCount = todos.filter(todo => todo.completed).length;
+
   return (
     <>
-    <div style={{width:"100%",height:'100vh'}} className="todo d-flex justify-content-center align-items-center">
-      <div style={{width:'1000px',height:'auto'}} className="todo-app bg-dark shadow rounded">
-       <div style={{backgroundColor:'#00b2a7'}} className='text-dark p-3 rounded shadow mb-4'> <h1 className='text-center'><i className="fa-solid fa-list-ul me-4"></i>My Todo List</h1></div>
-        <div className="input-todo d-flex justify-content-around align-items-center mb-3">
-          <input className='rounded shadow w-75 p-3' type="text" placeholder='Enter todo....' />
-          <button style={{backgroundColor:'#00b2a7'}} className='btn btn-success p-3'>Add</button>
-        </div>
-        <h4 className='text-light ms-5'>Total Completed Items : </h4>
-        <div  className="todo-list p-5 shadow">
-          <table className='table custom-table-bg'>
-            <thead>
-              <tr>
-                <th>Sl.NO</th>
-                <th>Todo</th>
-                <th>Completed</th>
-                <th>Delete</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>hai</td>
-                <td><button style={{backgroundColor:'#00b2a7'}} className='btn text-light'><i className="fa-solid fa-check"></i></button></td>
-                <td><button className='btn btn-danger'><i className="fa-solid fa-trash"></i></button></td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+      <div style={{ width: "100%", height: '100vh' }} className="todo d-flex justify-content-center align-items-center">
+        <div style={{ width: '700px', height: 'auto' }} className="todo-app bg-dark shadow rounded">
+          <div style={{ backgroundColor: '#00b2a7' }} className='text-dark p-3 rounded shadow mb-4'> <h1 className='text-center'><i className="fa-solid fa-list-ul me-4"></i>My Todo List</h1></div>
+          <div className="input-todo d-flex justify-content-around align-items-center mb-3">
+            <input className='rounded shadow w-75 p-2' type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder='Add todo....' />
+            <button onClick={handleAddTodo} style={{ backgroundColor: '#00b2a7' }} className='btn btn-success p-2'>Submit</button>
+          </div>
+          <h5 className='text-light ms-5'>Total Completed Items : {completedCount} </h5>
+          <div  className="todo-list p-5 shadow">
+            {todos?.length > 0 ?
+              <table  className='table custom-table-bg'>
+                <thead >
+                  <tr>
+                    <th>Todos</th>
+                    <th>Completed</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {todos?.map(item => (
+                    <tr key={item.id}>
+                      <td style={{ textDecoration: item.completed ? 'line-through' : 'none' }}>{item.text}</td>
+                      <td><button onClick={() => dispatch(toggleTodo(item.id))} style={{ backgroundColor: '#00b2a7' }} className='btn text-light'>{item.completed ? 'Undo' : 'Complete'}
+                      </button></td>
+                      <td><button onClick={() => dispatch(deleteTodo(item.id))} className='btn btn-danger'>Delete</button></td>
+                    </tr>
 
 
+                  ))
+                  }
+                </tbody>
+              </table>
+              :
+              <div style={{ fontSize: '2rem' }} className="text-center text-danger fw-bolder">
+                No todos!!!
+              </div>
+            }
+          </div>
+        </div>
       </div>
-
-    </div>
-      
     </>
   )
 }
